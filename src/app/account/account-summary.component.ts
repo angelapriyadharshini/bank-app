9c1bdb8313435d4213690fbdaaaea7c12bfb27e2
angelapriyadharshini <angela.amarapala@gmail.com>
2023-06-06 16:07:29 +0530

    building the initial layout

---
+++ src/app/account/account-summary.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-account-summary',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './account-summary.component.html',
+  styleUrls: ['./account-summary.component.css']
+})
+export class AccountSummaryComponent {
+
+}
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { AccountSummaryComponent } from './account/account-summary.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'account-summary' },
+  {
+    path: 'account-summary',
+    component: AccountSummaryComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
