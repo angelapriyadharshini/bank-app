b6bc7fd1ee73b703d53052267d2255d2665d58fd
angelapriyadharshini <angela.amarapala@gmail.com>
2023-06-12 15:08:36 +0530

    added transaction component and service

---
+++ src/app/transactions/transactions.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-transaction',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './transactions.component.html',
+  styleUrls: ['./transactions.component.css'],
+})
+export class TransactionsComponent {
+}
