d8f0ed6bfceeb2e21bf27bd613272e5a8f899ea8
angelapriyadharshini <angela.amarapala@gmail.com>
2023-06-12 17:21:59 +0530

    adding JSON date interceptor to providers

--- src/app/core/interceptors/index.ts
+++ src/app/core/interceptors/index.ts
@@ -1,12 +1,18 @@
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { HttpErrorInterceptorService } from './http-error-interceptor.service';
 import { importProvidersFrom } from '@angular/core';
+import { JsonDateInterceptorService } from './json-date-interceptor.service';
 
 export const httpInterceptorProviders = [
   importProvidersFrom(HttpClientModule),
   {
     provide: HTTP_INTERCEPTORS,
     useClass: HttpErrorInterceptorService,
+    multi: true,
+  },
+  {
+    provide: HTTP_INTERCEPTORS,
+    useClass: JsonDateInterceptorService,
     multi: true,
   },
 ];
